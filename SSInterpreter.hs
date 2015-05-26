ecead4bfa59d25dffa11d7cf0889485e9bbc12f6
rodrigoalves1 <raan@cin.ufpe.br>
2015-05-26 14:52:24 -0300

    Update SSInterpreter.hs

--- SSInterpreter.hs
+++ SSInterpreter.hs
@@ -1,31 +1,23 @@
 {-
-
 A basic interpreter for a purely functional subset of Scheme named SkimScheme.
 Part of this interpreter has been derived from the "Write Yourself a Scheme in
 48 Hours - An Introduction to Haskell through Example", by Jonathan Tang. It
 does not implement a number of Scheme's constructs. Moreover, it uses a
 different approach to implement mutable state within the language.
-
 The name "SkimScheme" refers to the stripped down nature of this interpreter.
 According to the New Oxford American Dictionary, "skim" can mean:
-
 (as a verb) ... read (something) quickly or cursorily so as to note only
 the important points.
-
 (as a noun) ... an act of reading something quickly or superficially. 
-
 "skimmed/skim milk" is milk from which the cream has been removed. 
-
 The name emphasizes that we do not want to cover the entire standard, small as
 it may be. Instead, we want to focus on some of the important aspects, taking a
 language implementer's point of view, with the goal of using it as a teaching
 tool. Many, many, many aspects of Scheme standards are not covered (it does not
 even support recursion!).
-
 Written by Fernando Castor
 Started at: August 28th 2012
 Last update: December 17th 2012
-
 -}
 
 module Main where
@@ -55,6 +47,16 @@ eval env (List (Atom "begin":[])) = return (List [])
 eval env (List (Atom "comment":[v])) = eval env Comment
 eval env (List (Atom "comment": l: ls)) = (eval env l) >>= (\v -> case v of { (error@(Error _)) -> return error; otherwise -> eval env (List (Atom "comment": ls))})
 eval env (List (Atom "comment":[])) = return (Comment)
+
+eval env (List (Atom "set!" : var@(Atom variavel) : expression )) = stateLookup env variavel >>= set env var expression
+--(begin (define a (+ 5 2)) (set! a 5))
+
+--eval env (List (Atom "let" : List bindings : body : [] )) = lett env bindings body  
+-- bindings =((variablei initi) . . . ) init sao expressoes
+-- exemplo (let ((x 2) (y 3)) (* x y))
+
+eval env (List (Atom "if" : test : consequent : alternate : [] )) = eval env test >>= iff env [consequent] [alternate]
+--runhaskell SSInterpreter.hs "(iff (lt? 2 5) (define a 5) (define a 2))"
 
 eval env lam@(List (Atom "lambda":(List formals):body:[])) = return lam
 -- The following line is slightly more complex because we are addressing the
@@ -165,6 +167,13 @@ instance Monad StateTransformer where
 -- Includes some auxiliary functions. Does not include functions that modify
 -- state. These functions, such as define and set!, must run within the
 -- StateTransformer monad. 
+--set :: StateT -> LispVal -> [LispVal] -> StateTransformer LispVal
+set env var args (Error t) = return (Error t)
+set env var args _ = define env (var:args)
+
+iff env [consequent] [alternate] (Bool True) = eval env consequent
+iff env [consequent] [] (Bool False) = return (Atom "")
+iff env [consequent] [alternate] (Bool False) = eval env alternate 
 
 car :: [LispVal] -> LispVal
 car [List (a:as)] = a
